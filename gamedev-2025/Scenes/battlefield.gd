ce20b057db9b131871d802225e8b67dad9d57129
Quilfort Frederiks <88137144+Quilfort@users.noreply.github.com>
2025-04-15 22:44:16 +0200

    Update Enemy and Add Spawner on Battlefield

---
+++ gamedev-2025/Scenes/battlefield.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func game_over():
+	%NorthSpawnTimer.start()
+
+func new_game():
+	%StartTimer.start()
+
+
+func _on_start_timer_timeout() -> void:
+	%NorthSpawnTimer.start()
+
+
+func _on_north_spawn_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	var enemy_location = %NorthSpawnLocation
+	enemy_location.progress_ratio = randf()
+	enemy.position = enemy_location.position
+	add_child(enemy)
--- gamedev-2025/Scripts/Enemy/enemy_basic.gd
+++ gamedev-2025/Scripts/Enemy/enemy_basic.gd
@@ -1,4 +1,9 @@
-extends CharacterBody2D
-
+extends RigidBody2D
 
 @export var movement_speed = 20.0
+
+func _ready():
+	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
