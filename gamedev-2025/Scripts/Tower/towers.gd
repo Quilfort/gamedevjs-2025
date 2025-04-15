b09aef21951143a7f24c2d9d58c2cb8428895d30
Quilfort Frederiks <88137144+Quilfort@users.noreply.github.com>
2025-04-15 21:35:58 +0200

    Add Towers and Movement with Enemy Pos

---
+++ gamedev-2025/Scripts/Tower/towers.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	turn()
+
+func turn():
+	var enemy_position = get_global_mouse_position()
+	%Turret.look_at(enemy_position)
