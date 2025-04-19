38d3094ea3b6bd8ee5c06a2208137b729978844d
Quilfort Frederiks <88137144+Quilfort@users.noreply.github.com>
2025-04-19 12:38:14 +0200

    Update Direction FOV Towers

--- gamedev-2025/Scripts/Tower/towers.gd
+++ gamedev-2025/Scripts/Tower/towers.gd
@@ -1,17 +1,53 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+enum FacingDirection { EAST, NORTH, WEST, SOUTH }
+
+# Angle map for the enum
+const DIRECTION_ANGLES := {
+	FacingDirection.EAST: 0.0,
+	FacingDirection.NORTH: 270.0,
+	FacingDirection.WEST: 180.0,
+	FacingDirection.SOUTH: 90.0
+}
+
+# Exported variables
+@export var facing_direction: FacingDirection = FacingDirection.SOUTH:
+	set(value):
+		facing_direction = value
+		_update_facing()
+
+@export var fov_angle: float = 90.0
+@export var fov_range: float = 200.0
+
+# Node references
+@onready var fov_area := $Base/FOVArea
+@onready var fov_polygon := $Base/FOVArea/CollisionPolygon2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_update_facing()
+	generate_fov_shape()
 
+func _update_facing() -> void:
+	var angle: float = DIRECTION_ANGLES[facing_direction]
+	fov_area.rotation_degrees = angle
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
+func generate_fov_shape():
+	var points: Array[Vector2] = []
+	var half_angle = deg_to_rad(fov_angle / 2.0)
+	var segments = 20
+
+	points.append(Vector2.ZERO) # center point
+
+	for i in range(segments + 1):
+		var angle = -half_angle + i * ((half_angle * 2.0) / segments)
+		var x = cos(angle) * fov_range
+		var y = sin(angle) * fov_range
+		points.append(Vector2(x, y))
+
+	fov_polygon.polygon = points
+
 func _physics_process(delta: float) -> void:
 	turn()
 
 func turn():
 	var enemy_position = get_global_mouse_position()
-	%Turret.look_at(enemy_position)
--- gamedev-2025/Scripts/central_cube.gd
+++ gamedev-2025/Scripts/central_cube.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+enum FacingDirection { EAST, NORTH, WEST, SOUTH }
+
 func _ready() -> void:
 	# Center the cube in the middle of the screen
 	position = get_viewport_rect().size / 2
+	set_tower_facing_directions()
+	
+func set_tower_facing_directions() -> void:
+	$NorthSide/NorthTowerBasic.facing_direction = FacingDirection.NORTH
+	$EastSide/EastTowerBasic.facing_direction = FacingDirection.EAST
+	$WestSide/WestTowerBasic.facing_direction = FacingDirection.WEST
+	$SouthSide/SouthTowerBasic.facing_direction = FacingDirection.SOUTH
